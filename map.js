3065391d742a7366dff267bffdafadee79cbf564
pranandarao <pranav.anandarao@gmail.com>
2020-10-24 11:23:56 -0400

    added mapbox

--- map.js
+++ map.js
@@ -2,9 +2,12 @@
 var map = L.map('map').setView([42.35, -71.08], 13);
 
 // load a tile layer
-L.tileLayer('http://tiles.mapc.org/basemap/{z}/{x}/{y}.png',
-    {
-    attribution: 'Tiles by <a href="http://mapc.org">MAPC</a>, Data by <a href="http://mass.gov/mgis">MassGIS</a>',
-    maxZoom: 17,
-    minZoom: 9
-    }).addTo(map);
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/satellite-v9',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'sk.eyJ1IjoicHJhbmFuZGFyYW8iLCJhIjoiY2tnbnR2YWR5MDVoYTJ5cXk4azhyZnlsNSJ9.5f4PYrU5R87SUGmkb3VDkQ', 
+    center: [42.408276, -85.372824]
+}).addTo(map);
