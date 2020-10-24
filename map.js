cc7c1d583577d6db877870d77961feccc1244ce3
pranandarao <pranav.anandarao@gmail.com>
2020-10-24 12:12:46 -0400

    changed stuff

--- map.js
+++ map.js
@@ -5,17 +5,10 @@ var map = L.map('map').setView([42.408276, -85.372824], 13);
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
-    id: 'mapbox/streets-v11',
+    id: 'mapbox/satellite-v9',
     tileSize: 512,
     zoomOffset: -1,
     accessToken: 'sk.eyJ1IjoicHJhbmFuZGFyYW8iLCJhIjoiY2tnbnR2YWR5MDVoYTJ5cXk4azhyZnlsNSJ9.5f4PYrU5R87SUGmkb3VDkQ'
-}).addTo(map);
-
-var circle = L.circle([42.408276, -85.372824], {
-    color: 'red',
-    fillColor: '#f03',
-    fillOpacity: 0.5,
-    radius: 500
 }).addTo(map);
 
 var marker = L.marker([42.408276, -85.372824]).addTo(map);
