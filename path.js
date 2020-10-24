742069e6c2610530c2aee2e2c5ba4464392bde3f
pranandarao <pranav.anandarao@gmail.com>
2020-10-24 15:10:20 -0400

    path

---
+++ path.js
@@ -0,0 +1,28 @@
+// initialize the map
+var map = L.map('map').setView([42.408276, -85.372824], 16);
+
+// load a tile layer
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/satellite-v9',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'sk.eyJ1IjoicHJhbmFuZGFyYW8iLCJhIjoiY2tnbnR2YWR5MDVoYTJ5cXk4azhyZnlsNSJ9.5f4PYrU5R87SUGmkb3VDkQ', 
+    center: [42.408276, -85.372824]
+}).addTo(map);
+
+var polyLine = L.polyline(dat, {color: 'red', weight: 1}).addTo(map);
+
+var clicks = [];
+
+function onMapClick(e) {
+    popup
+        .setLatLng(e.latlng)
+        .setContent("You clicked the map at " + e.latlng.toString())
+        .openOn(map);
+}
+
+
+
+map.on('click', onMapClick);
